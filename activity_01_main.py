cd75d7e7769f840d04b7fb88c390fa99d66e562f
Taran Badesha <tsingh18@rrc.ca>
2026-01-13 01:11:10 -0600

    completed client program

--- activity_01_main.py
+++ activity_01_main.py
@@ -2,30 +2,49 @@
 Description: A client program written to verify correctness of 
 the activity classes.
 """
-__author__ = "ACE Faculty"
+__author__ = "Taranpreet Singh"
 __version__ = "1.0.0"
 __credits__ = ""
 
 
+from library_item.library_item import LibraryItem
+from genre.genre import Genre
+
+
 def main():
-    """Test the functionality of the methods encapsulated 
+    """Test the functionality of the methods encapsulated
     in this project.
-    """ 
-    # In the statements coded below, ensure that any statement that could result 
-    # in an exception is handled.  When exceptions are 'caught', display the exception 
+    """
+    # In the statements coded below, ensure that any statement that could result
+    # in an exception is handled. When exceptions are 'caught', display the exception
     # message to the console.
 
     # 1. Code a statement which creates an instance of the LibraryItem class with valid inputs.
     # Use your own unique valid values for the inputs to the class.
+    try:
+        library_item = LibraryItem(
+            "The Hobbit",
+            "J.R.R. Tolkien",
+            Genre.FANTASY
+        )
+    except ValueError as error:
+        print(error)
 
-
-    # 2. Using the instance defined above, and the class Accessors, print 
+    # 2. Using the instance defined above, and the class Accessors, print
     # each of the attributes of the LibraryItem instance.
-
-    
+    try:
+        print(library_item.title)
+        print(library_item.author)
+        print(library_item.genre)
+    except ValueError as error:
+        print(error)
 
     # 3. Code a statement which creates an instance of the LibraryItem class with one or more invalid inputs.
     # Use your own unique valid values for the inputs to the class.
+    try:
+        LibraryItem("", "Unknown Author", Genre.FICTION)
+    except ValueError as error:
+        print(error)
 
 
 if __name__ == "__main__":
